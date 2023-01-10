aaec2f78f5e677bb3fcd5d38f08d9329375f50f1
alexandreklein <alexandreklein@unibw.de>
2023-01-10 23:23:50 +0100

    Changed OpCode from class to enum. Added "enum" for addressTypes and implemented some more AddressTypes.

--- src/Assembler/Interpreter/VirtualMachine.java
+++ src/Assembler/Interpreter/VirtualMachine.java
@@ -13,6 +13,22 @@ public class VirtualMachine {
     public static final int NUMBER_OF_REGISTERS = 16;
     public static final int WORD_SIZE = 4;
 
+    public class AddressType {
+        public static final int absolutAddress = 9;
+        public static final int smallDirectOperand = 0;
+        public static final int bigDirectOperand_Or_StackAddressingWithPlus = 8;
+        public static final int registerAddressing = 5;
+        public static final int relativeAddressingWithZero = 6;
+        public static final int relativeAddressingWithByte = 10;
+        public static final int relativeAddressingWithHalfword = 12;
+        public static final int relativeAddressingWithWord = 14;
+        public static final int indicatedRelativeAddressing = 4;
+        public static final int indirectAddressingWithByte = 11;
+        public static final int indirectAddressingWithHalfword = 13;
+        public static final int indirectAddressingWithWord = 15;
+        public static final int stackAddressingWithMinus = 7;
+    }
+
     // Condition Codes / Flags
     private boolean C, Z, N, V;
 
@@ -65,61 +81,148 @@ public class VirtualMachine {
         registers[SP_REGISTER] = value;
     }
 
-    private byte getNextByte() {
-        byte result = memory[getPC()];
+    private int getNextByte() {
+        int result = getByte(getPC());
         addOneToPC();
         return result;
     }
 
-    private int getNextOperand(int OperandSize) {
-        byte b = getNextByte();
+    private int getByte(int address) {
+        byte result = memory[address];
+        return result & 0xFF;
+    }
+
+    private int getNextHalfword() {
+        int a = getNextByte();
+        a = (a << 4) + getNextByte();
+        return a;
+    }
+
+    private int getHalfword(int address) {
+        int a = getByte(address);
+        a = (a << 4) + getByte(address);
+        return a;
+    }
+
+    private int getNextWord() {
+        int a = getNextHalfword();
+        a = (a << 8) + getNextHalfword();
+        return a;
+    }
+
+    private int getWord(int address) {
+        int a = getHalfword(address);
+        a = (a << 8) + getHalfword(address);
+        return a;
+    }
+
+    // Only use this for 1, 2 or 4 Bytes
+    private int getMemory(int address, int size) {
+        assert(size == 1 || size == 2 || size == 4);
+        return switch (size) {
+            case 1 -> getNextByte();
+            case 2 -> getNextHalfword();
+            case 4 -> getNextWord();
+            default -> -1;
+        };
+    }
+
+    private int getAddress(int address) {
+        return getMemory(address, 4);
+    }
+
+    private int getNextOperand(int operandSize) {
+        int b = getNextByte();
 
         int reg = (b & 0x0F);
-        int addressType = (b & 0xFF) >> 4;
+        int addressType = b >> 4;
         if (addressType < 4) addressType = 0; // We do this so we only have to use one case statement for every direct Operand Case
 
         int result = 0;
 
+        //TODO: Create enums for the switch case
         switch (addressType) {
             // Direct Operand between 0 and 63
-            case 0: {
+            case AddressType.smallDirectOperand: {
                 result = (b & 0x3F);
             } break;
-            // Stack Addressing by -!Rx
-            case 7: {
-                registers[reg] -= OperandSize;
+            // Register Addressing
+            case AddressType.registerAddressing: {
+                result = registers[reg];
+            } break;
+            // Relative Addressing a + !Rx, where a = 0
+            case AddressType.relativeAddressingWithZero: {
                 result = memory[registers[reg]];
             } break;
-            case 8: {
+            // Stack Addressing by -!Rx
+            case AddressType.stackAddressingWithMinus: {
+                registers[reg] -= operandSize;
+                result = memory[registers[reg]];
+            } break;
+            case AddressType.bigDirectOperand_Or_StackAddressingWithPlus: {
                 // Direct Operand bigger than 63
                 if (reg == 15) {
-                    for (int i = 0; i < OperandSize; i++) {
-                        byte op = getNextByte();
-                        result = (result << 4) + (op & 0xFF);
+                    for (int i = 0; i < operandSize; i++) {
+                        int op = getNextByte();
+                        result = (result << 4) + op;
                     }
                 }
                 // Stack Addressing by !Rx+
                 else {
                     result = memory[registers[reg]];
-                    registers[reg] += OperandSize;
-                }
+                    registers[reg] += operandSize;
+                } break;
             }
             // Absolute Addressing
-            case 9: {
+            case AddressType.absolutAddress: {
                 assert((b & 0x0F) == 15); //Not sure why the rest of the byte has to be 15
 
                 int address = 0;
-                for (int i = 0; i < OperandSize; i++) {
-                    byte op = getNextByte();
-                    address = (address << 4) + (op & 0xFF);
+                for (int i = 0; i < operandSize; i++) {
+                    int op = getNextByte();
+                    address = (address << 4) + op;
                 }
                 result = memory[address];
             } break;
-            // Register Addressing
-            case 5: {
-                result = registers[reg];
+            // Relative Addressing a + !Rx, where a fits into one byte
+            case AddressType.relativeAddressingWithByte: {
+                int a = getNextByte();
+                int address = registers[reg] + a;
+                result = memory[address];
             } break;
-            //TODO: Other cases
+            // Relative Addressing a + !Rx, where a fits into two byte
+            case AddressType.relativeAddressingWithHalfword: {
+                int a = getNextHalfword();
+                int address = registers[reg] + a;
+                result = memory[address];
+            } break;
+            case AddressType.relativeAddressingWithWord: {
+                int a = getNextWord();
+                int address = registers[reg] + a;
+                result = memory[address];
+            } break;
+            case AddressType.indicatedRelativeAddressing: {
+                //TODO: Implement me
+            } break;
+            case AddressType.indirectAddressingWithByte: {
+                int a = getNextByte();
+                int address1 = registers[reg] + a;
+                int address2 = getAddress(address1);
+                result = getMemory(address2, operandSize);
+            } break;
+            case AddressType.indirectAddressingWithHalfword: {
+                int a = getNextHalfword();
+                int address1 = registers[reg] + a;
+                int address2 = getAddress(address1);
+                result = getMemory(address2, operandSize);
+            } break;
+            case AddressType.indirectAddressingWithWord: {
+                int a = getNextWord();
+                int address1 = registers[reg] + a;
+                int address2 = getAddress(address1);
+                result = getMemory(address2, operandSize);
+            } break;
+            default: assert(false);
         }
 
         return result;
@@ -127,8 +230,8 @@ public class VirtualMachine {
 
     private void saveResult(int result) {
         //TODO: For now only handling Registers
-        byte b = getNextByte();
-        int addressType = (b & 0xFF) >> 4;
+        int b = getNextByte();
+        int addressType = b >> 4;
 
         switch (addressType) {
             case 5: {
@@ -142,16 +245,16 @@ public class VirtualMachine {
     //endregion
 
     public void executeOneInstruction() {
-        byte opcode = getNextByte();
+        int opcode = getNextByte();
 
         //opcode & 0xFF makes the Byte unsigned
-        switch(opcode & 0xFF) {
-            case 0x00: halt(); break;
-            case 0x92: cmp_b(); break;
-            case 0x93: cmp_h(); break;
+        switch(OpCode.find(opcode)) {
+            case HALT: halt(); break;
+            //case 0x92: cmp_b(); break;
+            //case 0x93: cmp_h(); break;
 
-            case 0xBF: add_b2(); break;
-            case OpCode.ADD_B3_Code: add_b3(); break;
+            case ADD_B2: add_b2(); break;
+            case ADD_B3: add_b3(); break;
         }
     }
 
--- src/Assembler/OpCode.java
+++ src/Assembler/OpCode.java
@@ -2,14 +2,26 @@ package Assembler;
 
 import static Assembler.OpCode.DataType.*;
 
-public class OpCode {
-    public String name;
-    public int opcode;
-    public int operands;
-    public DataType type;
-    public int length; //in byte
+public enum OpCode {
+    HALT("HALT", 0x00, 0, NONE, 1),
+    ADD_B2("ADD_B2", 0xBF, 2, BYTE, 3),
+    ADD_B3("ADD_B3", 0xC4, 3, BYTE, 4);
 
-    public OpCode(String n, int opC, int o, DataType t, int l) {
+    public final String name;
+    public final int opcode;
+    public final int operands;
+    public final DataType type;
+    public final int length; //in byte
+
+    public static OpCode find(int code) {
+        for (OpCode op : OpCode.values()) {
+            if (op.opcode == code) return op;
+        }
+        //TODO: Throw exception
+        return null;
+    }
+
+    OpCode(String n, int opC, int o, DataType t, int l) {
         name = n;
         opcode = opC;
         operands = o;
@@ -26,10 +38,4 @@ public class OpCode {
         NONE
         //TODO: CONV hat Byte/Wort als datentyp, müssen wir dies unterstützen?
     }
-
-    public static final OpCode HALT = new OpCode("HALT", 0x00, 0, NONE, 1);
-    public static final OpCode REI = new  OpCode("REI", 0x01, 0, NONE, 1);
-    public static final OpCode ADD_B3 = new OpCode("ADD_B", 0xC4, 2, BYTE, 3);
-
-    public static final int ADD_B3_Code = 0xC4;
 }
