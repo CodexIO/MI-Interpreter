8364ffc72654c10e8e9d6dd0c1b7148950022e56
Alexandre Klein <kleinalexandre>
2023-02-21 20:08:24 +0100

    Fixed StackAddressing so that it generates the Right Code

--- src/Assembler/AST_Nodes/StackAddress.java
+++ src/Assembler/AST_Nodes/StackAddress.java
@@ -5,9 +5,10 @@ public class StackAddress implements Operand {
     private int reg;
     private boolean plus;
 
-    public StackAddress(int r, boolean plus) {
-        assert(r >= 0 && r <= 15);
-        reg = r;
+    public StackAddress(int reg, boolean plus) {
+        assert(reg >= 0 && reg <= 15);
+        this.reg = reg;
+        this.plus = plus;
     }
 
     public int getReg() {
@@ -16,7 +17,7 @@ public class StackAddress implements Operand {
 
     @Override
     public byte[] generateMachineCode() {
-        byte b = plus ? (byte) (0x70 + reg) : (byte) (0x80 + reg);
+        byte b = plus ? (byte) (0x80 + reg) : (byte) (0x70 + reg);
 
         return new byte[]{b};
     }
