695b2dc24f380a1ae2199f0e3e4aee441497d830
Alexandre Klein <kleinalexandre>
2023-02-21 20:06:34 +0100

    CLI now supports the specification of a file to run.

--- src/CLI/Main.java
+++ src/CLI/Main.java
@@ -2,14 +2,53 @@ package CLI;
 
 import Interpreter.VirtualMachine;
 
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
 
     static VirtualMachine vm;
     static byte[] memory = new byte[VirtualMachine.MEMORY_LENGTH];
     static int[] registers = new int[16];
 
+    private static String readFile(String filename) {
+        File file = new File(filename);
+
+        try (FileReader fr = new FileReader(file)){
+            StringBuilder sb = new StringBuilder();
+            String s1;
+
+            // File reader
+            BufferedReader br;
+
+            // Buffered reader
+            br = new BufferedReader(fr);
+
+            // Initialize sl
+            sb.append(br.readLine());
+
+            // Take the input from the file
+            while ((s1 = br.readLine()) != null) {
+                sb.append("\n");
+                sb.append(s1);
+            }
+
+            return sb.toString();
+        } catch (Exception e) {
+            System.err.println("File: " + file.getAbsolutePath() + " could not be found or opened.");
+        }
+        return "";
+    }
+
     //TODO: Add printing after each executed Instruction
     public static void main(String[] args) {
+        boolean fileSpecified = false;
+        String fileInput = "";
+
         if (args.length == 0) {
             System.out.println("Please specify input state for the memory.");
             return;
@@ -17,32 +56,47 @@ public class Main {
         for (int i = 0; i < args.length; i++) {
             String s = args[i];
 
-            if (s.equals("-m")) {
-                String mem = args[++i];
-                String[] bytes = mem.split(" ");
+            switch (s) {
+                case "-m" -> {
+                    String mem = args[++i];
+                    String[] bytes = mem.split(" ");
 
-                int j = 0;
-                for (String b : bytes) {
-                    memory[j++] = (byte) (Short.parseShort(b, 16));
+                    int j = 0;
+                    for (String b : bytes) {
+                        memory[j++] = (byte) (Short.parseShort(b, 16));
+                    }
                 }
-            }
-            else if (s.equals("-r")) {
-                String regList = args[++i];
-                String[] regs = regList.split(" ");
+                case "-r" -> {
+                    String regList = args[++i];
+                    String[] regs = regList.split(" ");
 
-                int j = 0;
-                for (String r : regs) {
-                    registers[j++] = Integer.parseInt(r, 16);
+                    int j = 0;
+                    for (String r : regs) {
+                        registers[j++] = Integer.parseInt(r, 16);
+                    }
+                }
+                case "-f" -> {
+                    fileInput = readFile(args[++i]);
+                    fileSpecified = true;
                 }
             }
         }
 
-        vm = new VirtualMachine(memory, registers);
+        if (fileSpecified) {
+            System.out.println(fileInput);
 
-        System.out.println("Initial State: \n" + vm);
+            vm = new VirtualMachine(fileInput);
+            vm.run();
+            System.out.println("Final State: \n" + vm);
+        }
+        else {
+            vm = new VirtualMachine(memory, registers);
 
-        vm.run();
+            System.out.println("Initial State: \n" + vm);
 
-        System.out.println("Final State: \n" + vm);
+            vm.run();
+
+            System.out.println("Final State: \n" + vm);
+        }
     }
 }
