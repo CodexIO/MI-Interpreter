b01864f99cff5ffeea426ecebbf325f260d8717c
alexandreklein <alexandreklein@unibw.de>
2023-03-07 21:06:13 +0100

    This belongs to the commit where DataDefinition can now parse unknown labels

--- src/Assembler/AST_Nodes/AST_DataDefinition.java
+++ src/Assembler/AST_Nodes/AST_DataDefinition.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 public class AST_DataDefinition extends Command {
 
-    List<Byte> bytes;
-    List<String> labelsToPatch;
+    private List<Byte> bytes;
+    public List<String> labelsToPatch;
 
     public AST_DataDefinition(OpCode op, int line, int address, int beg, int end, List<Byte> bytes) {
         super(op, line, address, beg, end);
@@ -18,6 +18,18 @@ public class AST_DataDefinition extends Command {
     public AST_DataDefinition(OpCode op, int line, int address, int beg, int end, List<Byte> bytes, List<String> labelsToPatch) {
         this(op, line, address, beg, end, bytes);
         this.labelsToPatch = labelsToPatch;
+    }
+
+    public void patchAddress(int address) {
+        int[] addressBytes = new int[]{address >> 24, address >> 16, address >> 8, address};
+        int i = 0;
+        while (i < bytes.size()) {
+            if (bytes.get(i) == (byte)0xDD) break;
+            i++;
+        }
+        for (int j = 0; j < 4; j++) {
+            bytes.set(i + j, (byte) addressBytes[j]);
+        }
     }
 
     @Override
